67eef0cece323ec073b69f314f164aabdd2775fc
Akbostan <rutinislerhes.0@gmail.com>
2024-12-03 23:07:59 +0300

    d

--- lib/product/init/app_initialize.dart
+++ lib/product/init/app_initialize.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_learn/firebase_options.dart';
+import 'package:flutter_bloc_learn/product/service/firebase_options.dart';
 
 final class AppInitialize {
   Future<void> make() async {
